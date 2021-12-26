453bf801010307ef396abde09a87efa3d71e6ec5
MSS <manansahni567@gmail.com>
2021-12-26 17:41:10 +0530

    Made Destinations Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/HomePage/Home.js';
+import Destination from './Components/Destination/Destination';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Destination" element={<Destination />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,20 @@
 import "./Navbar.css";
+import { useNavigate } from "react-router-dom";
 import logo from "../../Images/logo.svg";
 
 function Navbar(props) {
+    const navigate=useNavigate();
     const {active}=props;
     return <div className="Navbar">
         <div>
             <img className="Logo" src={logo} alt="Logo" />
         </div>
-        <hr />
+        <hr className="rule" />
         <div className="Right">
-            <button className={"links"+(active===0?" active":"")}> <span className="number">00</span> HOME</button>
-            <button className={"links"+(active===1?" active":"")}> <span className="number">01</span> DESTINATION</button>
-            <button className={"links"+(active===2?" active":"")}> <span className="number">02</span> CREW</button>
-            <button className={"links"+(active===3?" active":"")}> <span className="number">03</span> TECHNOLOGY</button>
+            <button className={"links"+(active===0?" active":"")} onClick={() => {navigate("/")}}> <span className="number">00</span> HOME</button>
+            <button className={"links"+(active===1?" active":"")} onClick={() => {navigate("/Destination")}}> <span className="number">01</span> DESTINATION</button>
+            <button className={"links"+(active===2?" active":"")} onClick={() => {navigate("/Crew")}}> <span className="number">02</span> CREW</button>
+            <button className={"links"+(active===3?" active":"")} onClick={() => {navigate("/Technology")}}> <span className="number">03</span> TECHNOLOGY</button>
         </div>
     </div>
 }
